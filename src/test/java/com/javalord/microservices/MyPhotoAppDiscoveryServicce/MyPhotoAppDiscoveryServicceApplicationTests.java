ddd8289aaea1d357edc475d5e0a7e2c29f9db740
Christopher-OOC <olojedechristopher24@gmail.com>
2025-03-10 00:34:23 +0100

    comit:

---
+++ src/test/java/com/javalord/microservices/MyPhotoAppDiscoveryServicce/MyPhotoAppDiscoveryServicceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalord.microservices.MyPhotoAppDiscoveryServicce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPhotoAppDiscoveryServicceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
